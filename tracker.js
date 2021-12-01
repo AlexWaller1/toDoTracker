301a8ae6577f32d93768b73e4c6f5bd4499d5be5
Alex Waller <alex@artofalexwaller.com>
2021-12-01 18:13:24 -0500

    toDoList

--- tracker.js
+++ tracker.js
@@ -1,1 +1,7 @@
 console.log("Hello Oa");
+
+const toDoForm = document.getElementById("to-do-form");
+
+const toDoButton = document.getElementById("to-do-button");
+
+const toDoList = document.getElementById("to-do-list");

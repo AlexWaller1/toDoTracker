b88d910d5e3c134a71240a6baeabc95b0c2d2087
Alex Waller <alex@artofalexwaller.com>
2021-12-05 12:10:21 -0500

    displayTasks()

--- tracker.js
+++ tracker.js
@@ -6,7 +6,7 @@ const toDoForm = document.getElementById("to-do-form");
 
 const toDoButton = document.getElementById("to-do-button");
 
-const toDoList = document.getElementById("to-do-list");
+let toDoList = document.getElementById("to-do-list");
 
 const msg1 = document.getElementById("msg-1");
 
@@ -14,34 +14,43 @@ toDoButton.addEventListener("click", onSubmit);
 
 //................................................................
 
-// let tasksArray = [];
+let tasksArray = "";
 let data1 = localStorage.getItem("task-list");
 let data2 = JSON.parse(data1);
-let tasksArray = data2;
+
+if (data2 == null) {
+  tasksArray = [];
+} else {
+  tasksArray = data2;
+}
 
 let retrievedData = [];
+retrievedData = localStorage.getItem("task-list");
 
-function liMaker(text) {
-  msg1.innerHTML = "Get To Work!";
-  const li = document.createElement("li");
-  li.id = "list-item";
-  let deleteButton = document.createElement("button");
-  deleteButton.innerHTML = "Complete Task";
-  deleteButton.id = "delete-button";
+function displayTasks() {
+  for (let i = 0; i < tasksArray.length; i++) {
+    const li = document.createElement("li");
+    li.id = "list-item";
+    let deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Complete Task";
+    deleteButton.id = "delete-button";
 
-  li.appendChild(
-    document.createTextNode(`
-  ${text}`)
-  );
-  li.appendChild(deleteButton);
-  toDoList.appendChild(li);
+    li.appendChild(
+      document.createTextNode(`
+  ${tasksArray[i].name}`)
+    );
+    li.appendChild(deleteButton);
+    toDoList.appendChild(li);
 
-  deleteButton.addEventListener("click", function () {
-    toDoList.removeChild(li);
-
-    deleteButton = "";
-  });
+    deleteButton.addEventListener("click", function () {
+      toDoList.removeChild(li);
+      // localStorage.getItem("task-list").splice(i, 1);
+      deleteButton = "";
+    });
+  }
 }
+
+displayTasks();
 
 //................................................................
 
@@ -51,17 +60,20 @@ function onSubmit(e) {
   } else {
     e.preventDefault();
 
-    liMaker(toDoForm.value);
+    toDoList.innerHTML = "";
+    toDoList = document.getElementById("to-do-list");
+    // liMaker(toDoForm.value);
+    msg1.innerHTML = "Get To Work!";
 
     let obj = { name: `${toDoForm.value}`, done: false };
     tasksArray.push(obj);
 
+    displayTasks();
     // console.log(tasksArray);
     let parseTask = JSON.stringify(tasksArray);
     localStorage.setItem("task-list", parseTask);
 
     retrievedData = localStorage.getItem("task-list");
-
     console.log(retrievedData);
     console.log(tasksArray);
     console.log(tasksArray[0].done);
